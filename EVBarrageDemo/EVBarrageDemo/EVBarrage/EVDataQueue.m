9708eb15bb8a48c7547413d3e96884c3fd6e5323
Ever <wangyongbao2009@hotmail.com>
2019-05-27 19:56:58 +0800

    QueueNode

--- EVBarrageDemo/EVBarrageDemo/EVBarrage/EVDataQueue.m
+++ EVBarrageDemo/EVBarrageDemo/EVBarrage/EVDataQueue.m
@@ -8,14 +8,27 @@
 
 #import "EVDataQueue.h"
 
+@interface EVQueueNode : NSObject
+
+@property (nonatomic, strong) id value;
+@property (nonatomic, strong) id next;
+
+@end
+
+@implementation EVQueueNode
+
+@end
+
 static const dispatch_time_t TIME_OUT = 0.5; //信号量超时时间(单位：s)
 
 @interface EVDataQueue ()
 {
-    void ** _buffer; //二级指针，用来保存队列中所有的对象指针
+//    void ** _buffer; //二级指针，用来保存队列中所有的对象指针
     
-    int _front; //指向对首
-    int _rear; //指向对尾
+    
+    
+    EVQueueNode * _front; //指向对首
+    EVQueueNode * _rear; //指向对尾
     
     int _currentCapacity; //当前队列最大容量
     int _maxCapacity; //当前队列最大可扩展容量
@@ -39,7 +52,7 @@ static const dispatch_time_t TIME_OUT = 0.5; //信号量超时时间(单位：s)
         if (_currentCapacity >= _maxCapacity) {
             _currentCapacity = _maxCapacity;
         }
-        _buffer = calloc(_currentCapacity, sizeof(void *));
+//        _buffer = calloc(_currentCapacity, sizeof(void *));
     }
     return self;
 }
@@ -52,9 +65,9 @@ static const dispatch_time_t TIME_OUT = 0.5; //信号量超时时间(单位：s)
 }
 
 - (BOOL)isFull {
-    if ((_rear + 1) % _maxCapacity == _front) {
-        return YES;
-    }
+//    if ((_rear + 1) % _maxCapacity == _front) {
+//        return YES;
+//    }
     return NO;
 }
 
@@ -65,10 +78,21 @@ static const dispatch_time_t TIME_OUT = 0.5; //信号量超时时间(单位：s)
 //        dispatch_semaphore_signal(_semaphore);
         return NO;
     }
-    void *p = (__bridge_retained void *)obj;
-    _buffer[_rear % _maxCapacity] = p;
-    _rear = (_rear + 1) % _maxCapacity;
+//    void *p = (__bridge_retained void *)obj;
+//    _buffer[_rear % _maxCapacity] = p;
+//    _rear = (_rear + 1) % _maxCapacity;
     
+    EVQueueNode *node = [EVQueueNode new];
+    node.value = obj;
+    node.next = nil;
+    
+    _rear.next = node;
+    _rear = node;
+    
+    if (_front == nil) {
+        _front = node;
+    }
+        
 //    dispatch_semaphore_signal(_semaphore);
     
     return YES;
@@ -81,10 +105,13 @@ static const dispatch_time_t TIME_OUT = 0.5; //信号量超时时间(单位：s)
 //        dispatch_semaphore_signal(_semaphore);
         return nil;
     }
-    void *p = _buffer[_front];
-    id obj = (__bridge_transfer id)p;
-
-    _front = (_front + 1) % _maxCapacity;
+//    void *p = _buffer[_front];
+//    id obj = (__bridge_transfer id)p;
+//
+//    _front = (_front + 1) % _maxCapacity;
+    
+    id obj = _front.value;
+    _front = _front.next;
     
 //    dispatch_semaphore_signal(_semaphore);
     
@@ -96,8 +123,11 @@ static const dispatch_time_t TIME_OUT = 0.5; //信号量超时时间(单位：s)
 }
 
 - (void)freeAll {
-    free(_buffer);
-    _buffer = NULL;
+//    free(_buffer);
+//    _buffer = NULL;
+    
+    _front = nil;
+    _rear = nil;
 }
 
 @end
